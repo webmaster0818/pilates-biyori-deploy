49e50091966d31983ff3817eb0e95e71947cac4d
webmaster0818 <webmaster@mediax.biz>
2026-04-10 10:40:35 +0900

    全110エリアのtest-ikebukuro仕様をデプロイ環境に反映
    - ソースリポジトリでnpx next buildを実行
    - out/ディレクトリの内容をデプロイリポジトリに同期
    - 未反映だった20エリア（札幌、仙台、新宿、渋谷、池袋、銀座、東京、大宮、千葉、名古屋、静岡、新潟、大阪、京都、神戸、広島、岡山、福岡、熊本、沖縄）を含む全エリアを反映

--- _next/static/chunks/app/area/shimbashi/page-ca4dfb5f9bb30b89.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[7759],{},_=>{_.O(0,[2120,4049,8441,1255,7358],()=>_(_.s=4049)),_N_E=_.O()}]);
--- _next/static/chunks/app/layout-fb6df2f467609809.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[7177],{1101:(e,s,_)=>{Promise.resolve().then(_.t.bind(_,1290,23))},1290:()=>{}},e=>{e.O(0,[1741,8441,1255,7358],()=>e(e.s=1101)),_N_E=e.O()}]);
