622c0f4e98b366ff8240a9a9f1949221b82da723
webmaster0818 <webmaster@mediax.biz>
2026-04-10 16:04:08 +0900

    Deploy: policy pages and footer links

---
+++ _next/static/chunks/app/area/shirokanedai/page-665d9fd0bf5512df.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[513],{},_=>{_.O(0,[2619,253,8441,1255,7358],()=>_(_.s=253)),_N_E=_.O()}]);
---
+++ _next/static/chunks/app/benefits/page-ba4d831f4e786e95.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[5635],{},_=>{_.O(0,[2619,1534,8441,1255,7358],()=>_(_.s=1534)),_N_E=_.O()}]);
